1b0e070dd9e84311f087789a79afbf9634fa9a8c
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-03-28 18:20:43 +1100

    Initial model setup

---
+++ src/model.jl
@@ -0,0 +1,23 @@
+# One species
+# Three size classes
+# One location
+
+# Initial values
+# Available area [m²]
+k_area = 1000
+
+# Fraction of k_area filled with each size class corals []
+init_cover = [0.3, 0.2, 0.1]
+
+# Diameter size class bins [m]
+bins = [0.0, 0.01, 0.1, 0.5]
+
+# Diameter Linear Extension [m/year]
+Δd = [0.01, 0.03, 0.02]
+
+# Background Mortality rate [1/year]
+bm = [0.05, 0.03, 0.01]
+
+function timestep_iteration()
+
+end
