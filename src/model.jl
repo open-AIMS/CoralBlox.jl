94211bb685f380c67381bc29cf79a307575cd76d
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-03-28 18:39:59 +1100

    First dummy version

--- src/model.jl
+++ src/model.jl
@@ -1,13 +1,13 @@
+# One location
 # One species
 # Three size classes
-# One location
 
 # Initial values
 # Available area [m²]
 k_area = 1000
 
 # Fraction of k_area filled with each size class corals []
-init_cover = [0.3, 0.2, 0.1]
+rel_init_cover = [0.3, 0.2, 0.1]
 
 # Diameter size class bins [m]
 bins = [0.0, 0.01, 0.1, 0.5]
@@ -18,6 +18,29 @@ bins = [0.0, 0.01, 0.1, 0.5]
 # Background Mortality rate [1/year]
 bm = [0.05, 0.03, 0.01]
 
-function timestep_iteration()
+init_cover = k_area .* rel_init_cover
 
+function timestep_iteration(init_cover)
+    # First apply mortality to all size classes
+    init_cover .-= init_cover .* [0.05, 0.03, 0.01]
+
+
+    # Cover income from previous size class
+    # TODO
+    income_cover = [0, 0.01, 0.01] .* init_cover
+
+    # Cover loss to next size class
+    # TODO
+    loss_cover = [1, 0.01, 0] .* init_cover
+
+    # Cover internal growth
+    # TODO
+    internal_growth = [0, 0.02, 0.02] .* init_cover
+
+    # Settlers
+    settlers = [0.1 * init_cover[3], 0, 0]
+
+    @. init_cover += (income_cover + internal_growth - loss_cover + settlers)
+
+    return init_cover
 end
