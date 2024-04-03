34b3b5d8ea30e39e1d0164b43c4d79054f363bda
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-04-04 09:20:52 +1100

    WIP update model to accept multiple species

--- src/model.jl
+++ src/model.jl
@@ -1,75 +1,83 @@
-# One location
-# One species
-# Three size classes
+using YAXArrays
 
-# Initial values
+n_species = 2
+n_bins = 4
+axlist = (Dim{:species}(1:n_species), Dim{:size}(1:n_bins))
+
 # Available area [m²]
-k_max = 1000
+k_max = 10000
 
 # Settlers
-r = k_max * 0.4
+r = k_max .* [0.1, 0.15]
 
 # Fraction of k_max filled with each size class corals []
-rel_init_cover = [0.2, 0.2, 0.2, 0.01]
+rel_init_cover = YAXArray(axlist, [0.1 0.05 0.05 0.01; 0.15 0.07 0.03 0.02])
+@assert (sum(rel_init_cover) <= 1) "Sum of all relative cover should be smaller than or equal to 1"
 
 # Initial coral cover
 init_cover = k_max .* rel_init_cover
 
 # Diameter size class bins [m]
-bins = [0.0, 0.01, 0.2, 0.4, 0.5]
-n_bins = length(bins) - 1
+bins = [0.0 0.01 0.2 0.4 0.5; 0.0 0.01 0.2 0.4 0.5]
 
 # Diameter Linear Extension [m/year]
-# Large corals don't expand
-l = [0.005, 0.02, 0.02]
+l = YAXArray(axlist, [0.005 0.02 0.02 0.0; 0.004 0.017 0.023 0.0])
 
 # Background Mortality rate [1/year]
-bm = [0.02, 0.01, 0.01, 0.005]
-
+bm = YAXArray(axlist, [0.02 0.01 0.01 0.005; 0.019 0.012 0.013 0.007])
 
 """
 Fraction of k_max available.
 """
-function _k(cover::Vector{Float64})
-    return sum(cover) > k_max ? error("Problems") : 1 - (sum(cover) / k_max)
+function _k(cover::Array{Float64})
+    return sum(cover) > k_max ? error("k-area should not be greater than k_max") : 1 - (sum(cover) / k_max)
 end
 
 """
+Diameter proportionality factor of size class `s` corals with diameter between `di` and
+`df`. This factor, multiplied by the total cover `C_s` of size class `s` corals gives the
+area of the corals within that size class with diameter between `di` and `df`.
+
+# Arguments
 - `s` : Size class
+- `di`: Initial diameter
+- `df`: Final diameter
 """
-function _bins_factor(s, di, df)
-    return (df^3 - di^3) / (bins[s+1]^3 - bins[s]^3)
+function _diameter_factor(s, di, df)
+    @info "s = $s ; di = $di ; df = $df"
+    @info (df^3 - di^3) ./ (bins[:, s+1] .^ 3 .- bins[:, s] .^ 3)
+    return (df^3 - di^3) ./ (bins[:, s+1] .^ 3 .- bins[:, s] .^ 3)
 end
 
 """
-- `cover` : timesteps X sizeclass Matrix
+- `cover` : timesteps X species X size class
 - `t` : Time step of that iteration
 """
 function timestep_iteration(cover, t)
-    cover_t::Vector{Float64} = zeros(Float64, n_bins)
-    @info "Cover before iteration $t: $(cover[t-1, :])"
+    cover_t::Matrix{Float64} = zeros(Float64, n_species, n_bins)
+
     # Available area
-    k = _k(cover[t-1, :])
+    k = _k(cover[t-1, :, :])
+    k = _k(cover[1, :, :])
 
     # Settlers [area]
     ζ = r * log(1 + k)
-    @info "k: $k"
+
     # Survival rate
     survivals = 1 .- bm
-    #cover[t, :] .= cover[t, :] .* survivals
 
     # Increase in diameter
     D = l .* k
 
     small = 1
-    medium = 2:(n_bins-1)
+    medium = collect(2:(n_bins-1))
     large = n_bins
 
-    cover_t[small] = ζ
-    @. cover_t[medium] = cover[t-1, medium] * survivals[medium] * _bins_factor.(medium, bins[medium], (bins[medium.+1] - D[medium])) +
-                         cover[t-1, medium.-1] * survivals[medium.-1] * _bins_factor.(medium .- 1, bins[medium] - D[medium.-1], bins[medium])
-    cover_t[large] = cover[t-1, large] * survivals[large] +
-                     cover[t-1, large-1] * survivals[large-1] * 3 * D[large-1] * bins[4]^2 / (bins[large]^2 - bins[large-1]^2)
+    cover_t[:, small] .= ζ
+    @. cover_t[:, medium] = cover[t-1, :, medium] * survivals[:, medium] * _diameter_factor.([medium medium], bins[:, medium]', (bins[:, medium.+1] - D[:, medium])') +
+                            cover[t-1, :, medium.-1] * survivals[:, medium.-1] * _diameter_factor.(medium .- 1, bins[:, medium] - D[:, medium.-1], bins[:, medium])
+    @. cover_t[:, large] = cover[t-1, :, large] * survivals[:, large] +
+                           cover[t-1, :, large-1] * survivals[:, large-1] .* (3 .* D[:, large-1] .* bins[:, 4]^2) ./ (bins[:, large]^2 .- bins[:, large-1]^2)
 
     @info "Cover after iteration $t: $(cover_t)"
     return cover_t
@@ -79,13 +87,13 @@ end
 # What to do with juveniles going to medium size class when there's no space to grow?
 
 n_timesteps = 50
-cover = zeros(n_timesteps, n_bins)
-total_cover = zeros(n_timesteps)
-cover[1, :] = init_cover
-total_cover[1] = sum(cover[1, :])
+cover = zeros(n_timesteps, n_species, n_bins)
+total_cover = zeros(n_timesteps, n_species)
+cover[1, :, :] = init_cover
+total_cover[1, :] = sum(cover[1, :, :], dims=2)
 for t in 2:n_timesteps
-    cover[t, :] .= timestep_iteration(cover, t)
-    total_cover[t] = sum(cover[t, :])
+    cover[t, :, :] .= timestep_iteration(cover, t)
+    total_cover[t, :] = sum(cover[t, :, :], dims=2)
 end
 
 using Plots
