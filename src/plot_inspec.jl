84f9b4370a1fc86ff234480330eeb05ffc083f8b
Pedro Ribeiro de Almeida <pedroribalmeida@gmail.com>
2024-05-03 12:00:08 +1000

    Update coral_spec with params base on EcoRRAP data and CScape

--- src/plot_inspec.jl
+++ src/plot_inspec.jl
@@ -4,12 +4,15 @@ using Plots, Colors
 This function will plot one figure for each timestep
 """
 function plot_size_class(size_classes::Matrix{SizeClass}, timestep::Int64, coral_spec)
-    plots = Array{Plots.Plot}(undef, 6)
-    for func_group in 1:6
-        colors = sequential_palette(func_group * 360 / 6, 7)[2:end]
-        plots[func_group] = plot(title=coral_spec.f_groups[func_group], ylims=(1, 1e7))
+    n_species = length(coral_spec.f_groups)
+    n_bins = size(coral_spec.bins)[2] - 1
+    plots = Array{Plots.Plot}(undef, n_species)
+    for species in 1:n_species
+        colors = sequential_palette(species * 360 / n_species, n_bins + 1)[2:end]
+        plots[species] = plot(title=coral_spec.f_groups[species], ylims=(1, 1e7))
         n_corals::Float64 = 0.0
-        for (size, sc) in enumerate(size_classes[func_group, :])
+
+        for (size, sc) in enumerate(size_classes[species, :])
             _x = vcat([[cv.interval[1], cv.interval[2]] for cv in sc.cover_blocks]...)
             _y = vcat([fill(cv.diameter_density * (Δinterval(cv.interval)), 2) for cv in sc.cover_blocks]...)
             n_corals += sum(_y) / 2
